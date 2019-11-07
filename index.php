31e6899aa4294fb4d8e96053e854ab66ff453428
Abhishek Kumar <48324028+Abhishek-Kr-jh@users.noreply.github.com>
2019-11-07 09:33:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DBMS</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0">
+	<script type="text/javascript" src="jquery-3.3.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap\bootstrap-4.3.1-dist\css\bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap\bootstrap-4.3.1-dist\js\bootstrap.min.js"></script>
+
+</head>
+<body>
+     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+     	<div class="container-fluid">
+     	    <a class="navbar-brand"><img src="picture\output.png" width="100%" height="100%"></a>
+
+            <ul class="nav navbar-nav navbar-right">
+                <a href="#" class="btn btn-primary">Signup</a>
+                <a href="#" class="btn btn-primary">Login</a>
+                
+            </ul>
+
+     	   <!--  <ul class="nav navbar-nav navbar-right">
+     	    	<form>
+     	    		<div class="form-group">
+					<div class="input-group">
+					    <span class="input-group-addon"><i class="fa fa-envelope"> </i></span>
+					    <input type="email" class="form-control" placeholder="User name "> &nbsp;&nbsp;
+				   
+						<span class="input-group-addon"><i class="fa fa-key"> </i></span>
+						<input type="password" class="form-control" placeholder="Password"> &nbsp;&nbsp;
+                        <button type="submit" >submit</button>
+
+					</div>
+				</div>
+
+     	    	</form>
+     	    	
+                 <li><a href="#"><span class="glyphicon glyphicon-user"></span>Create account</a></li>
+              
+            </ul> -->
+     
+         </div>
+     </nav>
+    <!--  <div class="container">
+     	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+     		<ol class="carousel-indicaters">
+     			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+     		</ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="picture\Prime.png" alt="Los Angeles" style="width:100%;">
+                </div>
+
+                <div class="item">
+                   <img src="picture\maxresdefault.jpg" alt="Chicago" style="width:100%;">
+                </div>
+    
+                <div class="item">
+                   <img src="picture/images (1).jpg" alt="New york" style="width:100%;">
+                </div>
+            </div>
+              <!- Left and right controls ->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+     	</div>
+     </div> -->
+     <img src="picture\Prime.png" class="img-fluid">
+</body>
+</html>
